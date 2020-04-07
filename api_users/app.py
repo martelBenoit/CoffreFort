1b4aba4974e99cf9c61ea00eb0beef9a86aaec1c
martelBenoit <e1601417@etud.univ-ubs.fr>
2020-04-07 16:23:00 +0200

    ajout get/put/delete user

--- api_users/app.py
+++ api_users/app.py
@@ -21,7 +21,7 @@ users.create_index([('login',pymongo.ASCENDING)],unique=True)
 def authenticate():
         if request.authorization:
                 g.user = request.authorization['username']
-                print(request.authorization['password'])
+                g.password = request.authorization['password']
         else:
                 g.user = 'Anonymous'
 
@@ -35,7 +35,6 @@ def index():
 @app.route('/api/users', methods=['POST'])
 @flasgger.swag_from('docs/registration_user.yml')
 def new_user():
-        print(request.is_json)
         content = request.get_json()
         if verify_scheme(content):
                 user = {'login':content['LOGIN'],
@@ -51,8 +50,65 @@ def new_user():
                 else:
                         return jsonify(registration=True)
         else:
-                return jsonify(registration=False,reason="")
+                return jsonify(registration=False,reason="Invalid parameters")
 
+
+@app.route('/api/users/<login>', methods=['PUT'])
+@flasgger.swag_from('docs/change_password.yml')
+def change_password(login):
+        if(g.user == login):
+                user = users.find_one({"login":login})
+                if user != None:
+                        if verifyPassword(g.password,user['password']):
+                                content = request.get_json()
+                                password = content['PASSWORD']
+                                if len(password) < 6:
+                                        return jsonify(update=False,reason="Password length too short")
+                                if password != None:
+                                        try:
+                                                users.update({"login":login},{"login":login,"password":hashPassword(password)})
+                                        except Exception as ex:
+                                                return jsonify(update=False,reason=ex)
+                                        else:
+                                                return jsonify(update=True,reason="")
+                                else:
+                                        return jsonify(update=False,reason="Invalid parameter password")
+                        else:
+                                return jsonify(update=False,reason="Incorrect password")
+                else:
+                        return jsonify(update=False,reason="Incorrect login")
+        else:
+                return jsonify(update=False,reason="Permission denied")
+
+@app.route('/api/users/<login>', methods=['DELETE'])
+def delete(login):
+        if(g.user == login):
+                user = users.find_one({"login":login})
+                if user != None:
+                        if verifyPassword(g.password,user['password']):
+                                users.delete_one({"login":login})
+                                return jsonify(deleted=True)
+                        else:
+                                return jsonify(deleted=False,reason="Incorrect password")
+                else:
+                        return jsonify(deleted=False,reason="Incorrect login")
+        else:
+                return jsonify(deleted=False,reason="Permission denied")
+
+
+@app.route('/api/users/<login>', methods=['GET'])
+def info(login):
+        if(g.user == login):
+                user = users.find_one({"login":login})
+                if user != None:
+                        if verifyPassword(g.password,user['password']):
+                                return jsonify(login=user['login'])
+                        else:
+                                return jsonify()
+                else:
+                        return jsonify()
+        else:
+                return jsonify()
 
 def verify_scheme(test):
         try:
@@ -69,6 +125,7 @@ def hashPassword(password):
         return salt+key
 
 def verifyPassword(password, hash):
+        salt = hash[:32]
         keyPassword = hashlib.pbkdf2_hmac('sha256',password.encode('utf-8'),salt,100000)
         keyHash = hash[32:]
         if keyPassword == keyHash:
