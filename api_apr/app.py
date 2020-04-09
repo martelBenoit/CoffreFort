c7c743bd0b19eb445d7a909c1917239cf8deee79
martelBenoit <e1601417@etud.univ-ubs.fr>
2020-04-09 13:16:13 +0200

    ajout struct api apr

---
+++ api_apr/app.py
@@ -0,0 +1,21 @@
+import os
+import json
+from flask import(Flask, jsonify, request, g)
+import flasgger
+import zmq
+
+app = Flask(__name__)
+swagger = flasgger.Swagger(app)
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://tokendealer:5555")
+
+@app.route('/api', methods=['GET'])
+def new_user():
+        return jsonify(hello='world')
+
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5200, debug=True)
+       
